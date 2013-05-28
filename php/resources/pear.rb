669c88cd783598ae0ca854d451eb3efc0a2f9f0c
Adar Porat <hadar@mybuilding.org>
2013-05-28 09:56:50 -0400

    removed unsupported actions from php cookbook

--- php/resources/pear.rb
+++ php/resources/pear.rb
@@ -17,14 +17,3 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-
-default_action :install
-actions :install, :upgrade, :remove, :purge
-
-attribute :package_name, :kind_of => String, :name_attribute => true
-attribute :version, :default => nil
-attribute :channel, :kind_of => String
-attribute :options, :kind_of => String
-attribute :directives, :kind_of => Hash, :default => {}
-attribute :zend_extensions, :kind_of => Array, :default => Array.new
-attribute :preferred_state, :default => 'stable'
