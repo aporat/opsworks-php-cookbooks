95bccabd522123bfb74ff0ff342b35810d4ad09d
Adar Porat <hadar@mybuilding.org>
2013-05-28 16:50:01 -0400

    php app deployment

--- phpapp/recipes/deploy.rb
+++ phpapp/recipes/deploy.rb
@@ -1,5 +1,16 @@
 node[:deploy].each do |app_name, deploy|
 
+  opsworks_deploy_dir do
+    user deploy[:user]
+    group deploy[:group]
+    path deploy[:deploy_to]
+  end
+
+  opsworks_deploy do
+    deploy_data deploy
+    app application
+  end
+
   script "install_composer" do
     interpreter "bash"
     user "root"
