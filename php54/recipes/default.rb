a79af3a92b5305c22a244cfbaab3ddd264ce02ed
Adar Porat <hadar@mybuilding.org>
2013-05-28 13:16:10 -0400

    manually install php 5.4 on amazon ami

--- php54/recipes/default.rb
+++ php54/recipes/default.rb
@@ -18,12 +18,36 @@
 
 case node[:platform]
 
-  #when "amazon"
+  when "amazon"
   #node.set['php']['packages'] = ['php54', 'php54-devel', 'php54-cli', 'php54-snmp', 'php54-soap', 'php54-xml', 'php54-xmlrpc', 'php54-process', 'php54-mysql', 'php54-pdo', 'php54-gd', 'php54-imap', 'php54-mbstring']
-  #node.set['mysql']['server']['packages'] = %w{mysql55-server}
-  #node.set['mysql']['client']['packages'] = %w{mysql55}
+  node.set['mysql']['server']['packages'] = %w{mysql55-server}
+  node.set['mysql']['client']['packages'] = %w{mysql55}
   
-  when "rhel", "fedora", "suse", "amazon"
+    # add the webtatic repository
+  yum_repository "webtatic" do
+    repo_name "webtatic"
+    description "webtatic Stable repo"
+    url "http://repo.webtatic.com/yum/el6/x86_64/"
+    key "RPM-GPG-KEY-webtatic-andy"
+    action :add
+  end
+  
+  yum_key "RPM-GPG-KEY-webtatic-andy" do
+    url "http://repo.webtatic.com/yum/RPM-GPG-KEY-webtatic-andy"
+    action :add
+  end
+  
+  # get the metadata
+  execute "yum -q makecache" do
+    action :nothing
+  end
+  
+  # manually install php 5.4....
+  execute "yum install -y php54w php54w-devel php54w-cli php54w-snmp php54w-soap php54w-xml php54w-xmlrpc php54w-process php54w-mysql55 php54w-pecl-memcache php54w-pecl-apc php54w-pear php54w-pdo php54w-gd php54w-imap php54w-mbstring" do
+    action :nothing
+  end
+  
+  when "rhel", "fedora", "suse"
   # add the webtatic repository
   yum_repository "webtatic" do
     repo_name "webtatic"
@@ -47,6 +71,8 @@ case node[:platform]
   node.set['mysql']['server']['packages'] = %w{mysql55-server}
   node.set['mysql']['client']['packages'] = %w{mysql55}
   
+  include_recipe 'php'
+  
   when "debian"
     include_recipe "apt"
 	apt_repository "dotdeb-php54" do
@@ -56,6 +82,7 @@ case node[:platform]
 		key "http://www.dotdeb.org/dotdeb.gpg"
 		action :add
 	end
+	
+	include_recipe 'php'
 end
 
-include_recipe 'php'
