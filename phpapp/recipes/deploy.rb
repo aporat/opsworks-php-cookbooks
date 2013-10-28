9ad090958958e831565be5fd85cfeedac9634921
Adar Porat <hadar@mybuilding.org>
2013-10-28 09:50:16 -0400

    load opsworks private key into the ec2 instance root user

--- phpapp/recipes/deploy.rb
+++ phpapp/recipes/deploy.rb
@@ -1,4 +1,4 @@
-node[:deploy].each do |app_name, deploy|
+node[:deploy].each do |application, deploy|
   
   # use opsworks ssh key management and load the key into the ec2 instance
   prepare_git_checkouts(
@@ -20,6 +20,28 @@ node[:deploy].each do |app_name, deploy|
     group "root"
     mode 0644
   end
+
+  # use opsworks ssh key management and load the key into the ec2 instance. 
+  # it's helpful to have the deploy key loaded into the root user
+
+  # copy ssh key to root user
+  execute "touch /root/.ssh/id_deploy" do
+    ignore_failure true
+  end
+  
+  ssh_key = deploy[:scm][:ssh_key]
+  
+  execute "copy ssh_key" do
+    command "echo '#{ssh_key}' > /root/.ssh/id_deploy"
+  end
+  
+  execute "chmod 0600 /root/.ssh/id_deploy" do
+    ignore_failure true
+  end
+
+  # make sure the ssh key is loaded
+  execute "eval `ssh-agent -s`"
+  execute "ssh-agent bash -c 'ssh-add /root/.ssh/id_deploy'"
 
   # set apache2 hosts
   web_app app_name do 
