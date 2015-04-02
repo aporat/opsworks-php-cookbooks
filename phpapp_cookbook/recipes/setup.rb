0488e2178546768d39c466a871da94f11f67ea29
Adar Porat <aporat@users.noreply.github.com>
2015-04-01 21:23:04 -0400

    install composer and disable opcache fast shutdown

--- phpapp_cookbook/recipes/setup.rb
+++ phpapp_cookbook/recipes/setup.rb
@@ -8,3 +8,18 @@ execute "yum remove -y mysql-libs.x86_64" do
 end
 
 package "git"
+package "mysql55w"
+package "python-setuptools"
+
+# disable opcache fast shutdown
+execute "sed -i 's/opcache.fast_shutdown=1/opcache.fast_shutdown=0/g' /etc/php.d/opcache.ini" do
+    ignore_failure true
+end
+
+execute "curl -sS https://getcomposer.org/installer | php" do
+    ignore_failure true
+end
+
+execute "mv composer.phar /usr/local/bin/composer" do
+    ignore_failure true
+end
